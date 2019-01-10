b6e247fb5d4cf1ac1a8a7d844ef5e7359a72c331
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-10 19:13:47 +0000

    RED: returns fizz when num is divisile by 3

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,10 +1,10 @@
 import unittest
-from lib.fizzbuzz import *
+from lib.fizzbuzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
 
     def test_returns_fizz_with_num_divisibl3_by_3(self):
-        fizzbuzz = FizzBUzz()
+        fizzbuzz = FizzBuzz()
         self.assertEqual(fizzbuzz.says(3), 'Fizz')
 
 if __name__ == '__main__':
