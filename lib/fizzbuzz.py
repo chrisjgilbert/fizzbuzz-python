8cd5e1ecb2718787cde2ae9a055c4000cfc0ab88
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-10 19:24:29 +0000

    REFACTORED: __isDivisileBy5 private helper method added

--- lib/fizzbuzz.py
+++ lib/fizzbuzz.py
@@ -1,7 +1,10 @@
 class FizzBuzz:
 
     def says(self, number):
-        if number % 5 == 0:
+        if self.__isDivisibleBy5(number):
             return 'Buzz'
         else:
             return 'Fizz'
+
+    def __isDivisibleBy5(self, number):
+        return True if number % 5 == 0 else False
